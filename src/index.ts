de05bf0b0ffda204c53da267c3346f2c48338cbe
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-06-22 13:34:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ async function trySendNewsletter() {
   }
 
   if(invalidHour != currentHour && sendedNewsletter == false && currentHour == 16) {
-    const object = await axios.get('https://newsapi.org/v2/top-headlines?country=br&sources=google-news-br&apiKey=843840939423426e833acee9382d8e15')
+    const object = await axios.get('https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=843840939423426e833acee9382d8e15')
 
     const news_one = object.data.articles[0]
     const news_two = object.data.articles[1]
