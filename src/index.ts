d5bcac355447805b6101a3f255f0971803048aab
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-06-23 12:29:33 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ console.log(`[Core] Starting on ${currentHour}:${strftime('%m')}...`)
 
 async function trySendNewsletter() {
   if(allowDebug) {
-    console.log(`[Core] Trying... Current Hour : ${currentHour}, Invalid Hour : ${invalidHour}, sendedNewsletter : ${sendedNewsletter}, isValid: ${invalidHour != currentHour && sendedNewsletter == false && currentHour == 15}`)
+    console.log(`[Core] Trying... Current Hour : ${currentHour}, Invalid Hour : ${invalidHour}, sendedNewsletter : ${sendedNewsletter}, isValid: ${invalidHour != currentHour && sendedNewsletter == false && currentHour == 16}`)
   }
 
   if(sendedNewsletter) {
@@ -33,7 +33,7 @@ async function trySendNewsletter() {
     }, ((60 * 1000) * 60) * 2)
   }
 
-  if(invalidHour != currentHour && sendedNewsletter == false && currentHour == 15) {
+  if(invalidHour != currentHour && sendedNewsletter == false && currentHour == 16) {
     const object = await axios.get('https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=843840939423426e833acee9382d8e15')
 
     const news_one = object.data.articles[0]
